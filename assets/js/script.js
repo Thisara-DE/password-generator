77a2726311ae00cebce490f5c506ee77b8669231
Glorzo <thisaraeranga@gmail.com>
2022-01-11 22:47:54 -0600

    Commit for assistance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,36 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// Assignment code here
+generateBtn.addEventListener("click", getLength);
 
-  passwordText.value = password;
+// function to get length of the PW 8-128 characters
+var getLength = function() {
+  var pwLength = window.prompt("How many characters would you like your password to contain");
+  console.log("User entered the pW length to be " + pwLength);
+  pwLength = parseInt(pwLength);
+  var pwLengthMax = 128;
+  var pwLengthMin = 8;  
 
-}
+  if (pwLength < pwLengthMin || pwLength > pwLengthMax) {
+    console.log("number entered is not valid, try again");
+    return getLength;    
+  }
+  else
+  {
+    console.log("number entered is good");
+    return pwLength;
+  }
+};
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
