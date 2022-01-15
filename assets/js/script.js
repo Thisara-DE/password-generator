f1742b3a4d2de2f090e789801772e2929bdcd944
Glorzo <thisaraeranga@gmail.com>
2022-01-15 16:28:34 -0600

    added random character generation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,30 +4,25 @@ var generateBtn = document.querySelector("#generate");
 // Assignment code here
 // function to get length of the PW 8-128 characters and return the validated number as an integer
 var getLength = function() {
-  var pwLength = window.prompt("How many characters would you like your password to contain");
-  console.log("User entered the PW length to be " + pwLength);  
+  var pwLength = window.prompt("How many characters would you like your password to contain");  
   var pwLengthMax = 128;
   var pwLengthMin = 8;
   // pwLength = parseInt(pwLength);   
 
   if (pwLength < pwLengthMin || pwLength > pwLengthMax) {
     window.alert("Password length must be between 8-128 Characters");
-    console.log("number entered is not within the range");
     return getLength();    
   }
   else if (isNaN(pwLength)) {
-    window.alert("Password length must be provided as a number");
-    console.log("user entered non-numbers");
+    window.alert("Password length must be provided as a number");    
     return getLength();
   }
   else if (pwLength == "" || pwLength == null) {
-    window.alert("Password length cannot be empty");
-    console.log("User didn't enter anything");
+    window.alert("Password length cannot be empty");    
     return getLength();
   }
   else
-  {
-    console.log("number entered is valid");
+  {   
     // calling the next function to confirm if the user needs special characters
     confirmSpChar();      
     return pwLength;
@@ -36,63 +31,82 @@ var getLength = function() {
 
 // function to confirm that the user needs special characters in the PW
 var confirmSpChar = function() {
-  var SpChar = window.confirm("Click OK to confirm including special characters");
-  console.log("User presented with special char confirm")
+  var SpChar = window.confirm("Click OK to confirm including special characters");  
 
-  if (SpChar) {
-    console.log("user confirmed Special Characters");
+  if (SpChar) {    
     return true;
   }
-  else {
-    console.log("user denied Special Characters");
+  else {    
     return false;
   }
 };
 
 // function to confirm that the user needs special characters in the PW
 var confirmNumericChar = function() {
-  var NumChar = window.confirm("Click OK to confirm including numeric characters");
-  console.log("User presented with numeric character confirm");
+  var NumChar = window.confirm("Click OK to confirm including numeric characters");  
 
   if (NumChar) {
-    console.log("User confirmed numeric characters");
-    return true;
+        return true;
   }
-  else {
-    console.log("user denied numeric characters");
+  else {    
     return false;
   }
 };
 
 // function to confirm that the user needs lowercase characters in the PW
 var lowerCaseChar = function() {
-  var lCaseChar = window.confirm("Click OK to confirm including lowercase characters");
-  console.log("User presented with lowercase character confirm");
+  var lCaseChar = window.confirm("Click OK to confirm including lowercase characters");  
 
-  if (lCaseChar) {
-    console.log("User confirmed lowercase characters");
+  if (lCaseChar) {    
     return true;
   }
-  else {
-    console.log("user denied lowercase characters");
+  else {    
     return false;
   }
 };
 
 // function to confirm that the user needs upper characters in the PW
 var upperCaseChar = function() {
-  var upCaseChar = window.confirm("Click OK to confirm including uppercase characters");
-  console.log("User presented with uppercase character confirm");
+  var upCaseChar = window.confirm("Click OK to confirm including uppercase characters");  
 
-  if (upCaseChar) {
-    console.log("User confirmed uppercase characters");
+  if (upCaseChar) {    
     return true;
   }
-  else {
-    console.log("user denied uppercase characters");
+  else {    
     return false;
   }
 };
+
+var randomObj = {
+  special: getRandomSpChar,
+  number: getRandomNumber,
+  lower: getRandomLowerCase,
+  upper: getRandomUpperCase
+}
+
+var getRandomSpChar = function(confirmSpChar) {
+    if(confirmSpChar) {
+    var specialChar = '!#$%&^()*,[]{}./';
+    return specialChar[Math.floor(Math.random() * specialChar.length)];
+    }
+    else {
+      return null;
+    }
+}
+
+var getRandomNumber = function() {
+
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+var getRandomLowerCase = function() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+var getRandomUpperCase = function() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
 
 // eventListener is capturing the click and launching getLength function
 generateBtn.addEventListener("click", getLength);
